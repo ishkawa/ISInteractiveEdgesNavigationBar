bd7d15a18755d25b17d3a8fd7f51e4e69cd96eae
Yosuke Ishikawa <y@ishkawa.org>
2013-11-04 20:11:42 +0900

    refactored ISInteractiveEdgesNavigationBar

--- ISInteractiveEdgesNavigationBar/ISInteractiveEdgesNavigationBar.m
+++ ISInteractiveEdgesNavigationBar/ISInteractiveEdgesNavigationBar.m
@@ -4,37 +4,27 @@
 
 - (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event
 {
-    UIView *leftView = self.topItem.leftBarButtonItem.customView;
-    if ([self shouldPassEventAtPoint:point toView:leftView]) {
-        CGPoint convertedPoint = [leftView convertPoint:point fromView:self];
-        return [leftView hitTest:convertedPoint withEvent:event];
-    }
+    NSMutableArray *subviews = [NSMutableArray array];
+    [subviews addObjectsFromArray:self.topItem.leftBarButtonItem.customView.subviews];
+    [subviews addObjectsFromArray:self.topItem.rightBarButtonItem.customView.subviews];
     
-    UIView *rightView = self.topItem.rightBarButtonItem.customView;
-    if ([self shouldPassEventAtPoint:point toView:rightView]) {
-        CGPoint convertedPoint = [rightView convertPoint:point fromView:self];
-        return [rightView hitTest:convertedPoint withEvent:event];
-    }
-    
-    return [super hitTest:point withEvent:event];
-}
-
-- (BOOL)shouldPassEventAtPoint:(CGPoint)point toView:(UIView *)view
-{
-    if (!view || view.isHidden || !view.userInteractionEnabled || view.alpha == 0.f) {
-        return NO;
-    }
-    
-    BOOL containsPoint = NO;
-    for (UIView *subview in [view.subviews arrayByAddingObject:view]) {
-        CGRect convertedFrame = [subview convertRect:subview.frame toView:view];
-        if (CGRectContainsPoint(convertedFrame, point)) {
-            containsPoint = YES;
+    UIView *receiverView = nil;
+    for (UIView *subview in subviews) {
+        CGRect convertedRect = [self convertRect:subview.frame fromView:subview.superview];
+        BOOL isTouchable = subview.userInteractionEnabled && !subview.isHidden && subview.alpha > 0.f;
+        BOOL containsPoint = CGRectContainsPoint(convertedRect, point);
+        if (isTouchable && containsPoint) {
+            receiverView = subview;
             break;
         }
     }
     
-    return containsPoint;
+    if (receiverView) {
+        CGPoint convertedPoint = [self convertPoint:point toView:receiverView];
+        return [receiverView hitTest:convertedPoint withEvent:event];
+    }
+    
+    return [super hitTest:point withEvent:event];
 }
 
 @end
